bf5c3a3806bff5afe8e0255d853b7d1eeb1c2a25
m-jimenez127 <mjimenez.accur8@gmail.com>
2025-12-25 21:10:45 +0800

    Relayout Page Content

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,23 +1,22 @@
-import { Box, Flex } from '@chakra-ui/react'
-import Sidebar from './Sidebar'
-import Header from './Header'
+import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 const Layout = ({ children }) => {
+  const contentBg = useColorModeValue("gray.50", "gray.900");
   return (
     <Flex h="100vh" overflow="hidden">
       <Sidebar />
       <Flex direction="column" flex={1} overflow="hidden">
         <Header />
-        <Box flex={1} overflowY="auto" p={6}>
-          {children}
+        <Box bg={contentBg} flex={1} overflowY="auto">
+          <Box maxW="1200px" mx="auto" w="full" p={6}>
+            {children}
+          </Box>
         </Box>
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 
-export default Layout
-
-
-
-
+export default Layout;
