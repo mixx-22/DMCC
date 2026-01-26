c14074b1fa5f2cf28ccde39aaa03a087d73d3548
m-jimenez127 <mjimenez.accur8@gmail.com>
2026-01-26 22:49:25 +0800

    Default Status

--- src/context/DocumentsContext.jsx
+++ src/context/DocumentsContext.jsx
@@ -149,7 +149,7 @@ export const DocumentsProvider = ({ children }) => {
         code: documentData.code || "",
         type: documentData.auditType || "",
         standard: documentData.standard || "",
-        status: 0,
+        status: -1,
         auditors: [],
         organization: {},
       };
